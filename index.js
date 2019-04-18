172204eaa11a5949e1b8a726f18c71bd9b652841
NickisKnowledge <lifechangingstuff@gmail.com>
2019-04-18 12:08:03 -0500

    use with native JS obj

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code createUser() fn here 
+
+// using plain JS object
+
+function createUser(){
+  let user1 = {
+    username: "Henry", 
+    age: 22, 
+    hobby: 'play instruments'
+  }
+
+  let list = document.getElementById('list');
+  list.innerHTML = `
+    <tr>
+      <td>${user1.username}</td>
+      <td>${user1.age}</td>
+      <td>${user1.hobby}</td>
+    </tr>
+  `
+
+}
