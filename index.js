0f75e2cc9318ef7e3dbf05fe6f6e755e0e1c570f
NickisKnowledge <lifechangingstuff@gmail.com>
2019-04-18 12:22:25 -0500

    use fn syntax

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 // code createUser() fn here 
 
-// using plain JS object
+// using Function syntax
+
+function User(username, age, hobby) {
+  this.username = username;
+  this.age = age;
+  this.hobby = hobby;
+}
+
+User.prototype.description = function() {
+  // console.log(`User ${this.username} is ${this.age} and likes to ${this.hobby}`)
+  return `User ${this.username} is ${this.age} and likes to ${this.hobby}`
+}
 
 function createUser(){
-  let user1 = {
-    username: "Henry", 
-    age: 22, 
-    hobby: 'play instruments'
-  }
+  // let user1 = {
+  //   username: "Henry", 
+  //   age: 22, 
+  //   hobby: 'play instruments'
+  // }
+
+  let user1 = new User('Henry', 22, 'play instruments')
+  let user2 = new User('JS Love', 21, 'coding in JavaScript')
 
   let list = document.getElementById('list');
   list.innerHTML = `
     <tr>
-      <td>${user1.username}</td>
-      <td>${user1.age}</td>
-      <td>${user1.hobby}</td>
+      <td>${user1.description()}</td>
+  
+    </tr>
+    <tr>
+      <td>${user2.description()}</td>
     </tr>
   `
 
